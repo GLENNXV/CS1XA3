44e94c43af61c9d8cfd044d0072590d1660cab7a
idkkk <xvshuren@gmail.com>
2020-02-11 18:35:04 -0500

    ok

--- project01/project_analyze.sh
+++ project01/project_analyze.sh
@@ -2,7 +2,40 @@
 # author:shuren xu
 # macid:xus83
 
-script_input(){
-    echo "please enter something"
-    read input
+fixmeLog(){
+    if [ -f "fixme.log" ]
+    then
+        rm -f fixme.log
+    fi
+    for i in `find ./project01 -type f`
+    do
+        echo $i
+        if [ -n `grep "#FIXME" $i | tail -1` ]
+        then
+            echo $i >> project01/fixme.log
+        fi
+    done
 }
+
+features(){
+    echo "please enter the feature you need as follow"
+    echo "'fixmeLog' "
+    echo "please be careful about the uppercases"
+    read input
+    echo "the feature will be activated is $input"
+    echo "anytime to run a feature just enter the name of the feature"
+    echo "-----------------------------------------------------------"
+    echo "for help enter 'features'"
+    echo "-----------------------------------------------------------"
+    echo "running"
+    echo "-----------------------------------------------------------"
+    eval $input
+    echo "-----------------------------------------------------------"
+    echo "done"
+}
+
+features
+
+
+
+
